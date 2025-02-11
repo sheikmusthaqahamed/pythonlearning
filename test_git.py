a400a9180c676a1d56fcc508cbd5c53532065a1d
sheik musthaq ahamed <sheik.musthaqahamed2@cognizant.com>
2025-02-11 12:11:51 +0530

    refactor test_git.py to remove age checks and add range printing

--- test_git.py
+++ test_git.py
@@ -1,9 +1,4 @@
 
-age = int(input("Enter your age: "))
-name = "Sheik"
-if age == 10:
-    print("Hi Sheik 10 yrs old")
-elif age >=11 and age <= 20:
-    print("Hi Sheik2")
-elif name == "Sheik":
-    print("Hi Sheik3")
+print(list(range(3,10)))
+for num in range(3,10):
+    print(num)
