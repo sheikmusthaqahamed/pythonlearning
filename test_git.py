e2a61749ade48f6fd23ab0997be74aab2179cd97
sheik musthaq ahamed <sheik.musthaqahamed2@cognizant.com>
2025-02-14 12:04:41 +0530

    valueerror

--- test_git.py
+++ test_git.py
@@ -1,14 +1,26 @@
-def test():
-    
-   
-    try:
-        return (12/0)
-    except Exception as e:
-        print("e1")
-        return e
-    finally:
-        print("rgvfb")
-        #return ("Finally Block")
+list_of_list = ["sbf",23,"grvf",["fwedgv",[2,44,"tbg"],"3rfe"],4]
+list2 = []
+for value in list_of_list:
+    if type(value) == list:
+        for val2 in value:
+            if type(val2)==list:
+               for num in val2:
+                list2.append(num)
+            else:
+                list2.append(val2)
+    else:
+        list2.append(value)
 
-#ONLY ONE RETURN IS RETURNED
-print(test())
+print(list_of_list)
+print(list2)
+
+a,b,c,d,e = ["ec","Ed","2de","wd"]
+
+#     a,b,c,d,e = ["ec","Ed","2de","wd"]
+#     ^^^^^^^^^
+# ValueError: not enough values to unpack (expected 5, got 4)
+
+
+#     a,b,c = ["ec","Ed","2de","wd"]
+#     ^^^^^
+# ValueError: too many values to unpack (expected 3)
